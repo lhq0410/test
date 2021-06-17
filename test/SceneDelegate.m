c8c976c7c6f7ef4f9463a047b02782d0268de285
lihaiqiang <2564597760@qq.com>
2021-06-17 20:56:38 +0800

    提交修改

--- test/SceneDelegate.m
+++ test/SceneDelegate.m
@@ -15,9 +15,14 @@
 
 
 - (void)scene:(UIScene *)scene willConnectToSession:(UISceneSession *)session options:(UISceneConnectionOptions *)connectionOptions {
-    // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-    // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-    // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+    
+    
+    
+    
+    
+    
+    // 流口水京东方临时登记反馈了
+    
 }
 
 
